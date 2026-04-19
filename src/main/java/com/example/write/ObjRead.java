2b5f98fc9c5ef9f44aef69148bc35662ffabda8c
tianshaofei <1058051552@qq.com>
2026-04-19 17:40:12 +0800

    提交4

--- src/main/java/com/example/write/ObjRead.java
+++ src/main/java/com/example/write/ObjRead.java
@@ -2,13 +2,13 @@ package com.example.write;
 
 /**
  * Email:1058051552@qq.com
- * Date 2026/4/19 17:29
+ * Date 2026/4/19 17:38
  * Description:
  *
  * @author tianshaofei
  */
-public class ObjWrite {
+public class ObjRead {
     public static void main(String[] args) {
-        System.out.println("测试写入");
+        System.out.println("测试读取");
     }
 }
