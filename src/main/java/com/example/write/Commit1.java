19c283fc7bb486b43c2654f13592dc4e46b40f1f
tianshaofei <1058051552@qq.com>
2026-04-19 17:55:52 +0800

    commit1-2

--- src/main/java/com/example/write/Commit1.java
+++ src/main/java/com/example/write/Commit1.java
@@ -2,9 +2,13 @@ package com.example.write;
 
 /**
  * Email:1058051552@qq.com
- * Date 2026/4/19 17:54 
- * Description: 
+ * Date 2026/4/19 17:54
+ * Description:
  *
  * @author tianshaofei
- */public class Commit1 {
+ */
+public class Commit1 {
+    public static void main(String[] args) {
+        System.out.println("commit1");
+    }
 }
