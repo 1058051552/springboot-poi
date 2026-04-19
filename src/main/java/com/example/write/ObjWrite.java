750d92f6a0a0db3c280f8036298fca5cbb19296d
tianshaofei <1058051552@qq.com>
2026-04-19 17:37:23 +0800

    Default Changelist

---
+++ src/main/java/com/example/write/ObjWrite.java
@@ -0,0 +1,14 @@
+package com.example.write;
+
+/**
+ * Email:1058051552@qq.com
+ * Date 2026/4/19 17:29
+ * Description:
+ *
+ * @author tianshaofei
+ */
+public class ObjWrite {
+    public static void main(String[] args) {
+        System.out.println("测试提交");
+    }
+}
